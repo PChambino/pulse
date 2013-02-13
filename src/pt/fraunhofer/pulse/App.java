64161784c7229b6de1a6585ecc6a69df359f04d8
Pedro Chambino <pchambino@gmail.com>
2013-02-13 15:45:37 +0000

    Cascade classifier has minimum size for performance.

--- src/pt/fraunhofer/pulse/App.java
+++ src/pt/fraunhofer/pulse/App.java
@@ -17,19 +17,19 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 public class App extends Activity implements CvCameraViewListener {
 
     private static final String TAG = "Pulse::App";
-    private static final Scalar RED = new Scalar(255, 0, 0, 255);
-    private static final Scalar BLUE = new Scalar(0, 0, 255, 255);
 
     private CameraBridgeViewBase camera;
-    private CascadeClassifier detector;
+    private CascadeClassifier faceDetector;
     
     private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -51,10 +51,10 @@ public class App extends Activity implements CvCameraViewListener {
         File dir = getDir("cascade", Context.MODE_PRIVATE);
         File file = createFileFromResource(dir, R.raw.lbpcascade_frontalface, "xml");
         
-        detector = new CascadeClassifier(file.getAbsolutePath());
-        if (detector.empty()) {
+        faceDetector = new CascadeClassifier(file.getAbsolutePath());
+        if (faceDetector.empty()) {
             Log.e(TAG, "Failed to load cascade classifier");
-            detector = null;
+            faceDetector = null;
         }
 
         dir.delete();
@@ -117,13 +117,24 @@ public class App extends Activity implements CvCameraViewListener {
         super.onDestroy();
     }
 
+    private static final Scalar RED = new Scalar(255, 0, 0, 255);
+    private static final Scalar BLUE = new Scalar(0, 0, 255, 255);
+
+    private static final float minFaceSizePercentage = 0.3f;
+
     private Mat gray;
     private Mat output;
+    private Point center;
+    private Size minFaceSize;
+    private Size maxFaceSize;
 
     @Override
     public void onCameraViewStarted(int width, int height) {
         gray = new Mat();
         output = new Mat();
+        center = new Point();
+        minFaceSize = new Size(minFaceSizePercentage * width, minFaceSizePercentage * height);
+        maxFaceSize = new Size();
     }
 
     @Override
@@ -136,15 +147,29 @@ public class App extends Activity implements CvCameraViewListener {
     public Mat onCameraFrame(Mat frame) {
         MatOfRect faces = new MatOfRect();
         
-        if (detector != null) {
+        if (faceDetector != null) {
             Imgproc.cvtColor(frame, gray, Imgproc.COLOR_RGB2GRAY);
-            detector.detectMultiScale(gray, faces);
+            faceDetector.detectMultiScale(gray, faces, 1.1, 2, 0, minFaceSize, maxFaceSize);
         }
 
         frame.copyTo(output);
 
         for (Rect face : faces.toArray()) {
-            Core.rectangle(output, face.tl(), face.br(), RED);
+            Core.rectangle(output, face.tl(), face.br(), RED, 4);
+            
+            // forehead point
+            center.x = face.tl().x + face.size().width * 0.5;
+            center.y = face.tl().y + face.size().width * 0.2;
+            Core.circle(output, center, 4, BLUE, 8);
+
+            // left cheek point
+            center.x = face.tl().x + face.size().width * 0.7;
+            center.y = face.tl().y + face.size().width * 0.6;
+            Core.circle(output, center, 4, BLUE, 8);
+
+            // right cheek point
+            center.x = face.tl().x + face.size().width * 0.3;
+            Core.circle(output, center, 4, BLUE, 8);
         }
 
         return output;
