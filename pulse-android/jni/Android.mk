51f89c0e4f3819f299b9b303aabdaeb3af4e4ea4
erik-smit <smite@zylon.net>
2013-09-29 06:28:54 +0200

    [exec] make: *** No rule to make target obj/local/armeabi-v7a/objs/pulse/profiler/profiler.o'.  Stop.

--- pulse-android/jni/Android.mk
+++ pulse-android/jni/Android.mk
@@ -2,14 +2,14 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-include libs/OpenCV-2.4.5-android-sdk/sdk/native/jni/OpenCV.mk
+include /home/smite/dev/opencv/platforms/build_android_arm/install/sdk/native/jni/OpenCV.mk
 
 LOCAL_SRC_FILES  := pt_fraunhofer_pulse_Pulse_Face.cpp
 LOCAL_SRC_FILES  += pt_fraunhofer_pulse_Pulse.cpp
 LOCAL_SRC_FILES  += Pulse.cpp
 LOCAL_SRC_FILES  += EvmGdownIIR.cpp
 LOCAL_SRC_FILES  += ext/opencv.cpp
-LOCAL_SRC_FILES  += profiler/profiler.cpp
+LOCAL_SRC_FILES  += profiler/Profiler.cpp
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_LDLIBS     += -llog -ldl
 
